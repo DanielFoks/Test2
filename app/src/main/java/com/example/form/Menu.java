335ffd347649459c427a71c2549ef9faad3ed509
Dmitry Samchuk <enet_00@mail.ru>
2015-12-20 13:34:53 +0200

    TEST34rr23523

--- app/src/main/java/com/example/form/Menu.java
+++ app/src/main/java/com/example/form/Menu.java
@@ -31,6 +31,9 @@ public class Menu extends Dialog implements OnClickListener{
 
     private int HELLO2;
     private int NEW2;
+    private int NEW21;
+    private int NEW22;
+    private int NEW23;
 
 
     private GridLayout.LayoutParams butParams;
