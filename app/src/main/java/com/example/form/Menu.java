4002f8a1403fac15507a99a088b32740483675e3
Dmitry Samchuk <enet_00@mail.ru>
2015-12-20 13:34:07 +0200

    TEST34r

--- app/src/main/java/com/example/form/Menu.java
+++ app/src/main/java/com/example/form/Menu.java
@@ -29,7 +29,7 @@ public class Menu extends Dialog implements OnClickListener{
     private Dialog dialog;
     public static double heightCenter;
 
-    private int HELLO;
+    private int HELLO2;
     private int NEW2;
 
 
