19c7ae6e719d4d58e5915163c9e39a5e42f83b43
Dmitry Samchuk <enet_00@mail.ru>
2015-12-20 14:13:56 +0200

    HELlO234

--- app/src/main/java/com/example/form/Menu.java
+++ app/src/main/java/com/example/form/Menu.java
@@ -30,7 +30,7 @@ public class Menu extends Dialog implements OnClickListener{
     public static double heightCenter;
 
     private String qwe;
-    private String SUKA245;
+    private String SUKA245432;
 
     private GridLayout.LayoutParams butParams;
 
