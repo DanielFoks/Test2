762b6525eb08037e84f6b9b9fe1bbf5b51966c41
Danil <foks.daniel95@gmail.com>
2015-12-20 14:27:56 +0200

    QWE

--- app/src/main/java/com/example/form/Menu.java
+++ app/src/main/java/com/example/form/Menu.java
@@ -29,7 +29,7 @@ public class Menu extends Dialog implements OnClickListener{
     private Dialog dialog;
     public static double heightCenter;
 
-    private int JOPA;
+    private String RILO;
 
     private GridLayout.LayoutParams butParams;
 
